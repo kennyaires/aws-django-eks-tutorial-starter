1b143fc0db66c8567a043177d2f53764775983d9
Kenny <kennyaires@gmail.com>
2023-08-01 23:49:16 -0300

    Added VPC

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "regon" {
   description = "AWS region to deploy resources to"
   default     = "sa-east-1"
 }
+
+variable "prefix" {
+  description = "Prefix to be assigned to all resources"
+  default     = "django"
+}
---
+++ infra/vpc.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0.1"
+
+  name = "${var.prefix}-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+  database_subnets = ["10.0.20.0/24", "10.0.21.0/24", "10.0.22.0/24"]
+
+  enable_nat_gateway           = true # to access outside/ internet
+  single_nat_gateway           = true # single one nat gateway to make it cheaper, ideally should have each one for each different zone
+  enable_dns_hostnames         = true
+  create_database_subnet_group = true
+}
+
+# tags help kubernetes to identify the subnets
+public_subnet_tags = {
+  "kubernetes.io/cluster/${var.prefix}-cluster" = "shared"
+  "kubernetes.io/role/elb"                      = 1 # load balancer
+}
+
+private_subnet_tags = {
+  "kubernetes.io/cluster/${var.prefix}-cluster" = "shared"
+  "kubernetes.io/role/elb"                      = 1 # load balancer
+}
